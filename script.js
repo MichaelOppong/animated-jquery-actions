d05c8cbfb06c06812ac252ef44c44d69a4fca4de
Michael Oppong <michael@scripted.org>
2018-01-02 20:14:08 +0000

    jQuery Actions

---
+++ script.js
@@ -0,0 +1,39 @@
+// JavaScript File
+$( document ).ready(function() {
+
+$("#show-div").click(function() {
+    $("img").show();
+});
+
+$("#hide-div").click(function() {
+    $("img").hide();
+});
+
+$("#toggle-div").click(function() {
+    $("img").toggle();
+});
+
+$("#slideUp-div").click(function() {
+    $("img").slideUp();
+});
+
+$("#slideDown-div").click(function() {
+    $("img").slideDown();
+});
+
+$("#slideToggle-div").click(function() {
+    $("img").slideToggle();
+});
+
+$("#fadeIn-div").click(function() {
+    $("img").fadeIn();
+});
+
+$("#fadeOut-div").click(function() {
+    $("img").fadeOut();
+});
+
+$("#fadeToggle-div").click(function() {
+    $("img").fadeToggle();
+});
+});
